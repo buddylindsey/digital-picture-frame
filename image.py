3403621ac3ec27ed82f14ca06e21940da568ebec
Buddy Lindsey, Jr <buddy@buddylindsey.com>
2017-02-14 21:01:27 -0600

    use a split view for images

--- image.py
+++ image.py
@@ -3,6 +3,7 @@ import os
 from random import randrange
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QFrame
 from PyQt5.QtGui import QIcon, QPixmap, QPainter
 from PyQt5.QtCore import QPoint, Qt
@@ -23,32 +24,53 @@ class MainWindow(QtWidgets.QMainWindow):
         self.initUI()
 
     def initUI(self):
+        main_widget = QtWidgets.QWidget()
+        self.setCentralWidget(main_widget)
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        hbox = QtWidgets.QHBoxLayout()
+
+        left = QtWidgets.QFrame()
+        left.setFrameShape(QtWidgets.QFrame.StyledPanel)
+
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.setImage()
+        left.setLayout(self.vbox)
+
+        right = QtWidgets.QFrame()
+        right.setFrameShape(QtWidgets.QFrame.StyledPanel)
+
+        splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        splitter.addWidget(left)
+        splitter.addWidget(right)
+
+        hbox.addWidget(splitter)
+
         screen_resolution = app.desktop().screenGeometry()
 
-        self.label = QLabel()
-        self.setImage()
-        self.setCentralWidget(self.label)
+        #timer = QtCore.QTimer(self)
+        #timer.timeout.connect(self.setImage)
+        #timer.setInterval(1000)
+        #timer.start()
 
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.setImage)
-        timer.setInterval(1000)
-        timer.start()
+        main_widget.setLayout(hbox)
 
-        #self.show()
-        self.showFullScreen()
+        self.show()
+        #self.showFullScreen()
 
     def setImage(self):
         print('set image')
         images = [f for f in os.listdir() if '.jpg' in f]
 
-        image = images[randrange(0, len(images))]
+        #image = images[randrange(0, len(images))]
 
-        pixmap = QPixmap(image)
-        #pm = pixmap.scaled(1920, 1080, Qt.KeepAspectRatio)
-        self.label.setPixmap(pixmap)
+        for image in images:
+            label = QLabel()
+            pixmap = QPixmap(image)
+            pm = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
+            label.setPixmap(pm)
+            self.vbox.addWidget(label)
 
 
 if __name__ == '__main__':
