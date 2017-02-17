1e9dd2d61fdac99940f34ac58b1e7e50a406e605
Buddy Lindsey, Jr <buddy@buddylindsey.com>
2017-02-16 21:11:50 -0600

    Make a slideshow and add menus

--- image.py
+++ image.py
@@ -11,10 +11,55 @@ from PyQt5.QtCore import QPoint, Qt
 
 app = QApplication.instance()
 
+
+class SlideShowWindow(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        self.label = QtWidgets.QLabel()
+        self.set_image()
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.set_image)
+        timer.setInterval(1000)
+        timer.start()
+
+        self.showFullScreen()
+
+    def set_image(self):
+        images = [f for f in os.listdir() if '.jpg' in f]
+
+        image = images[randrange(0, len(images))]
+
+        screen_resolution = app.desktop().screenGeometry()
+
+        pixmap = QPixmap(image)
+        pm = pixmap.scaled(
+            screen_resolution.width(), screen_resolution.height(),
+            Qt.KeepAspectRatio)
+        self.label.setPixmap(pm)
+
+        def keyPressEvent(self, event):
+            if event.key() == QtCore.Qt.Key_Escape:
+                self.close()
+            return super().keyPressEvent(event)
+
+        def eventFilter(self, event):
+            if event.type() == QtCore.QEvent.KeyPress:
+                return True # means stop event propagation
+            else:
+                return QtWidgets.QDialog.eventFilter(self, event)
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        app.main_window = self
+        #app.main_window = self
 
         self.title = 'PyQt5 image'
         self.left = 10
@@ -28,6 +73,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(main_widget)
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.buildMenu()
 
         hbox = QtWidgets.QHBoxLayout()
 
@@ -59,11 +105,31 @@ class MainWindow(QtWidgets.QMainWindow):
         self.show()
         #self.showFullScreen()
 
-    def setImage(self):
-        print('set image')
-        images = [f for f in os.listdir() if '.jpg' in f]
+    def buildMenu(self):
+        exitAction = QtWidgets.QAction(QtGui.QIcon('exit24.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtWidgets.qApp.quit)
 
-        #image = images[randrange(0, len(images))]
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        run = menubar.addMenu('Run')
+
+        slide_show_action = QtWidgets.QAction('Slide Show', self)
+        slide_show_action.setShortcut('Ctrl+R')
+        slide_show_action.setStatusTip('Start Slideshow')
+        slide_show_action.triggered.connect(self.start_slide_show)
+        run.addAction(slide_show_action)
+
+    def start_slide_show(self):
+        window = SlideShowWindow(self)
+        window.exec_()
+
+    def setImage(self):
+        images = [f for f in os.listdir() if '.jpg' in f]
 
         for image in images:
             label = QLabel()
