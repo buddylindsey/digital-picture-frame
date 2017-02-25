cf5d8737ef6b2bd663736e11460e9a746d34cbbf
Buddy Lindsey, Jr <blindsey@summitesp.com>
2017-02-24 23:00:11 -0600

    Add very basics of django to embedded webserver

--- src/web.py
+++ src/web.py
@@ -1,9 +1,40 @@
+import os
+import sys
 from PyQt5 import QtCore
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from django.conf import settings
+from django.core.servers.basehttp import run, get_internal_wsgi_application
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+DEBUG = True
+SECRET_KEY = os.urandom(32)
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+application = get_wsgi_application()
+
 
 class WebServer(QtCore.QObject):
     @QtCore.pyqtSlot()
     def run(self):
-        self._server = HTTPServer(('127.0.0.1', 8080), SimpleHTTPRequestHandler)
-        self._server.serve_forever()
+        run('127.0.0.1', 8080, application)
